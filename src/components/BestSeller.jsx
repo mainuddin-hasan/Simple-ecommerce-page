23617142b62f216c04465039cee2985fcd64dacc
mainuddin-hasan <mainuddinctg50419@gmail.com>
2023-12-28 23:40:46 +0600

    3rd commit

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -21,23 +21,26 @@ const BestSeller = () => {
     const totalItems = products.length;
 
     return (
-        <div className="mt-40 flex gap-5">
-            <div className="mx-auto">
-                <img className='w-[700px] h-[400px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNicAA6WEjIlMHz8azt5ep71jTv465hocuVR206HETwQ&s" alt="Best Selling" />
-            </div>
-            <div className="mx-auto">
-                <div className="flex gap-3 mx-auto">
-                    <h1 className="text-red-600 font-bold">Best Selling products</h1>
-                    <Pagination
-                        count={Math.ceil(totalItems / itemsPerPage)}
-                        page={currentPage}
-                        onChange={(event, newPage) => setCurrentPage(newPage)}
-                    />
+        <div className="w-11/12 mx-auto">
+            <div className="w-11/12 mt-40 flex flex-col lg:flex-row gap-5 items-center mx-auto">
+                <div className="mx-auto">
+                    <img className='w-[700px] h-[400px]' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNicAA6WEjIlMHz8azt5ep71jTv465hocuVR206HETwQ&s" alt="Best Selling" />
                 </div>
-                
-                <BestSaleItemList saleData={currentItems} />
+                <div className="mx-auto">
+                    <div className="flex gap-3 mx-auto">
+                        <h1 className="text-red-600 font-bold">Best Selling products</h1>
+                        <Pagination
+                            count={Math.ceil(totalItems / itemsPerPage)}
+                            page={currentPage}
+                            onChange={(event, newPage) => setCurrentPage(newPage)}
+                        />
+                    </div>
+
+                    <BestSaleItemList saleData={currentItems} />
+                </div>
             </div>
         </div>
+
     );
 };
 
